bc7ed5b846045fac2518cb50c34517f9bcab82b2
Haleh <halehnk1999@gmail.com>
2024-07-18 01:52:23 +0330

    adding different parts of the project

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 const toggler = document.querySelector(".nav__toggler");
 
-// toggler.addEventListener("click", function () {
-//   const navList = document.querySelector(".nav__list");
-//   navList.classList.contains("hidden-list")
-//     ? navList.classList.remove("hidden-list")
-//     : navList.classList.add("hidden-list");
-//   toggler.classList.contains("toggled")
-//     ? toggler.classList.remove("toggled")
-//     : toggler.classList.add("toggled");
-// });
+toggler.addEventListener("click", function () {
+  const navList = document.querySelector(".nav__list");
+  navList.classList.contains("hidden-list")
+    ? navList.classList.remove("hidden-list")
+    : navList.classList.add("hidden-list");
+  toggler.classList.contains("toggled")
+    ? toggler.classList.remove("toggled")
+    : toggler.classList.add("toggled");
+});
 
 const tabs = document.querySelectorAll(".tab");
 const tabContents = document.querySelectorAll(".tab-content");
